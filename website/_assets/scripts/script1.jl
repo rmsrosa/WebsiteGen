f5d83a3eedca1c2da587049a7c46f0f2e5bfcbb5
Ricardo Rosa <rmsrosa@gmail.com>
2021-01-15 21:55:39 -0300

    remove linting warning

--- website/_assets/scripts/script1.jl
+++ website/_assets/scripts/script1.jl
@@ -2,8 +2,8 @@ using LinearAlgebra # HIDE
 using Random:seed!  # HIDE
 seed!(0)            # HIDE
                     # HIDE
-x = randn(5)
-y = randn(5)
+x = Random.randn(5)
+y = Random.randn(5)
 
 for i in 1:5
     println(rpad("*"^i, 10, '-'), round(dot(x, y), digits=1))
